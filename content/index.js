9489f5c70329d39c9e1488322c2522065c2bcb70
谭乾仕 <tanqianshi@gzrb.com>
2023-02-02 12:37:51 +0800

    init

---
+++ content/index.js
@@ -0,0 +1,13 @@
+// 论文目录
+export default [
+    {
+        mdFile:'hello.md', //content目录下md文档
+        title:'Normal Mapping',//列表中展示的标题
+        author:'Hao Yin',
+        desc:'3 4-Credit Extension',// 描述
+        img:'/icom.png',// static目录下图片的路径
+        date:'20221010',// 发表日期
+        publisher:'Nature', // 出版商
+
+    }
+]
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nuxt-content :document="page" />
+    <Tutorial />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  async asyncData({ $content }) {
+    const page = await $content("hello").fetch();
+
+    return {
+      page,
+    };
+  },
+};
+</script>
