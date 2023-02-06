6dd4d077f4fd794dfc85204602ca409176ad1bdf
谭乾仕 <tanqianshi@gzrb.com>
2023-02-06 15:05:24 +0800

    save

--- content/index.js
+++ content/index.js
@@ -3,9 +3,9 @@ export default [
     {
         mdFile:'hello', //content目录下md文档文件名
         title:'ObjectFolder 2.0: A Multisensory Object Dataset for Sim2Real Transfer',//列表中展示的标题
-        author:'Hao Yin',
+        author:'Hao Yin', // 作者
         desc:'3 4-Credit Extension',// 描述
-        img:'/assets/1.jpg',// static目录下图片的路径
+        img:'/assets/1.jpg',// 列表封面：static目录下图片的路径
         date:'20221010',// 发表日期
         publisher:'Nature', // 出版商
 
